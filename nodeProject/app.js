eaaf4322b71426432de50ae57f574c3c28af73d6
bfarmerUMD <bfarmer@umd.edu>
2020-11-18 09:16:05 -0500

    Added nodeProject

---
+++ nodeProject/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.send(JSON.stringify({ message: "This is directly from my Node server" }))
+})
+
+app.post('/', function (req, res) {
+    res.send('Got a POST request')
+})
+
+app.put('/user', function (req, res) {
+    res.send('Got a PUT request at /user')
+})
+
+app.delete('/user', function (req, res) {
+    res.send('Got a DELETE request at /user')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
